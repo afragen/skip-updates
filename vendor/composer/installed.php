04fd8304efff75bc16abc0975d97fd895c67791b
Andy Fragen <andy@thefragens.com>
2021-02-17 10:04:23 -0800

    composer update for release

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,24 +1,24 @@
 <?php return array (
   'root' => 
   array (
-    'pretty_version' => 'dev-develop',
-    'version' => 'dev-develop',
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
     'aliases' => 
     array (
     ),
-    'reference' => 'cc98303ea27e17e6cb173a6949d42bb7cbcd5de7',
+    'reference' => '97a9300c45aad23bf6cf38649e56f437c580add8',
     'name' => 'afragen/skip-updates',
   ),
   'versions' => 
   array (
     'afragen/skip-updates' => 
     array (
-      'pretty_version' => 'dev-develop',
-      'version' => 'dev-develop',
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
       'aliases' => 
       array (
       ),
-      'reference' => 'cc98303ea27e17e6cb173a6949d42bb7cbcd5de7',
+      'reference' => '97a9300c45aad23bf6cf38649e56f437c580add8',
     ),
   ),
 );
